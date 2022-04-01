03f5f5b153f857d470298356ef8ff2a3a5522fb8
kalashneupane <102482227+kalashneupane@users.noreply.github.com>
2022-04-01 19:20:25 -0400

    Pyhton practice

---
+++ IntroProject.py
@@ -0,0 +1,38 @@
+print ("Hello World")
+print("Hello World")
+greeting= "Hello World"
+print (greeting)
+if 5 > 2:
+ print("Five is greater than two!")  
+if 5 > 2:
+        print("Five is greater than two!") 
+if  5 > 2:
+    print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!")
+        #This is a comment
+print("Hello World")
+x=5
+y= "John"
+print (x)
+print (y)
+x=4 # x is of type int
+x="Sally" # x is now of type str
+print(x)
+x = str(3)   # x will be '3'
+y = int (3)   # y will be 3
+z = float (3) # z will be 3.0
+print(x)
+print(y)
+print(z)
+x = "python is awesome"
+print(x)
+x = 5
+y = 10
+print (x + y)
+
+
+
+
+
+
