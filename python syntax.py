7a79b80596c541f58d0ac38df0fec110f24e4e84
kalashneupane <102482227+kalashneupane@users.noreply.github.com>
2022-04-03 16:24:41 -0400

    pythonsyntax

--- python syntax.py
+++ python syntax.py
@@ -1,1 +1,5 @@
-print("This is new hello world")
+print("This is new hello world") #code follows this type of syntax
+if 5 > 2:
+ print("Five is greater than two!") #Identation space before print
+x = 5 
+y = "hello world" #assisgn value to variable
