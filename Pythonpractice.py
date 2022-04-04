eb6c520f240a255d7907fcca8afc6fa219508b07
kalashneupane <102482227+kalashneupane@users.noreply.github.com>
2022-04-04 09:16:10 -0400

    pythonpractice

---
+++ Pythonpractice.py
@@ -0,0 +1,37 @@
+print("This is new hello world") #code follows this type of syntax
+if 5 > 2:
+ print("Five is greater than two!") #Identation space before print
+x = 5 
+y = "hello world" #assisgn value to variable
+print("This is new hello world") #code follows this type of syntax
+if 5 > 2:
+ print("Five is greater than two!") #Identation space before print
+x = 5 
+y = "hello world" #assisgn value to variable
+print(x)
+print(y)
+x = 4 # x is type int
+x = "Sally" # x is now of type str
+print(x)
+x = str(3) # x will be "3" casting exercise
+y = int(3) # y will be 3
+z = float(3) # z will be 3.0
+print(x)
+print(y)
+print(z)
+x = 5 #know int
+y = "john" #know str
+print(type(x))
+print(type(y))
+x = 'john' #same output
+print(x)
+x = "john" #same output
+print(x)
+a = 4
+A = "Sally"
+# case sensentive A will not overwrite a
+print(a)
+print(A)
+
+
+
