0d5cbb9ae09f32ed8e9b22a83f0213559a050893
kalashneupane <102482227+kalashneupane@users.noreply.github.com>
2022-04-17 18:43:18 -0400

    String

--- PythonChapters.py
+++ PythonChapters.py
@@ -36,4 +36,36 @@ x = bytearray(5) #bytearray
 print(type(x))
 x = memoryview(bytes(5))
 print(type(x))
+x = 1 #integer
+y = 2.8 #float
+z = 1j #complex
+a = float(x)
+b = int(y)
+c = complex(z)
+print(a)
+print(b)
+print(c)
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+print(x)
+print(y)
+print(z)
+print(w)
+a = "Hello World" #string
+print(a)
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.''' #multilinestring
+print(a)
+txt = "The best things in life are free!"
+print("free" in txt)
+txt = "The best things in life are free!"
+if "free"in txt:
+    print("Yes,'free' is present")
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+print("No,'expensive' is Not present")
 
